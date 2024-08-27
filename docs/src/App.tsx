76db649fa9084ac02350a17d82a4e9c22d176dea
sa <shashiyadavcm@gmail.com>
2024-08-27 17:23:13 +0530

    Docs

---
+++ docs/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-800">
+      <h1 className="text-[13vw] leading-none tracking-tighter">Docs</h1>;
+    </div>
+  );
+}
+
+export default App;
