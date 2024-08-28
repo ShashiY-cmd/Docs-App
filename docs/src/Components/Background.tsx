5b0f64edfab40e257e0c6c405ced20f9d5ed585c
sa <shashiyadavcm@gmail.com>
2024-08-28 23:07:56 +0530

    Done

--- docs/src/Components/Background.tsx
+++ docs/src/Components/Background.tsx
@@ -1,13 +1,15 @@
 export default function Background() {
   return (
     <>
-      <div className="absolute top-[5%]  w-full py-10 flex justify-center text-xl text-zinc-600 font-semibold">
-        Documents.
+      <div className="absolute w-full h-screen z-[2]">
+        <div className="absolute top-[5%]  w-full py-10 flex justify-center text-xl text-zinc-600 font-semibold">
+          Documents.
+        </div>
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900 text-[13vw] leading-none tracking-tighter">
+          Docs.
+        </h1>
+        ;
       </div>
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900 text-[13vw] leading-none tracking-tighter">
-        Docs.
-      </h1>
-      ;
     </>
   );
 }
---
+++ docs/src/Components/Foreground.tsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import Card from "./Card";
+
+function Foreground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      desc: "The Brown fox is running at a speed  of lamborgini",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Downlaod Now", tagColor: "green" },
+    },
+    {
+      desc: "The Brown fox is running at a speed  of lamborgini",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Downlaod Now", tagColor: "blue" },
+    },
+    {
+      desc: "The Brown fox is running at a speed  of lamborgini",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
+    },
+  ];
+  return (
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5 "
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
+    </div>
+  );
+}
+
+export default Foreground;
